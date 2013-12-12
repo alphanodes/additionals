d700e60ce8d47d947b26d155a0803ce2eb3913f5
Alexander Meindl <a.meindl@alphanodes.com>
2013-12-12 14:31:36 +0100

    deprecated render calls fixed

--- lib/redmine_tweaks/hooks.rb
+++ lib/redmine_tweaks/hooks.rb
@@ -4,13 +4,13 @@
 module RedmineTweaks
 
   class RedmineTweaksHookListener < Redmine::Hook::ViewListener
-    render_on(:view_issues_new_top, :partial => 'new_ticket_message.html.erb')
+    render_on(:view_issues_new_top, :partial => 'new_ticket_message')
 
-    render_on(:view_projects_show_right, :partial => 'project_overview.html.erb')
-    render_on(:view_account_login_bottom, :partial => 'login_text.html.erb')
+    render_on(:view_projects_show_right, :partial => 'project_overview')
+    render_on(:view_account_login_bottom, :partial => 'login_text')
 
-    render_on(:view_projects_show_sidebar_bottom, :partial => 'global_sidebar.html.erb')
-    render_on(:view_issues_sidebar_queries_bottom, :partial => 'global_sidebar.html.erb')
+    render_on(:view_projects_show_sidebar_bottom, :partial => 'global_sidebar')
+    render_on(:view_issues_sidebar_queries_bottom, :partial => 'global_sidebar')
 
     def view_layouts_base_html_head(context = {})
       if Setting.plugin_redmine_tweaks['external_urls'] == '1'

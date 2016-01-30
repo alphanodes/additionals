48c18106cde5e3edd3164b1cbb16a811bf8ae2d2
Alexander Meindl <a.meindl@alphanodes.com>
2016-01-30 15:50:19 +0100

    secure fix for send_file

--- app/controllers/garfield_controller.rb
+++ app/controllers/garfield_controller.rb
@@ -6,7 +6,8 @@ class GarfieldController < ApplicationController
   before_action :require_login
 
   def show
-    local_path = "#{Rails.root}/tmp/_garfield_#{params[:name]}.jpg".freeze
+    filename = sanitize(params[:name])
+    local_path = "#{Rails.root}/tmp/_garfield_#{filename}.jpg"
     send_file(local_path, disposition: 'inline', type: 'image/jpeg', x_sendfile: true)
   end
 end

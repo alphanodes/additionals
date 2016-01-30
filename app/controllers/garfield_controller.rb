82d498a7a4551d08f29f3434c3bcb57106f5d6f1
Alexander Meindl <a.meindl@alphanodes.com>
2016-01-30 15:27:23 +0100

    Another fix for local filename

--- app/controllers/garfield_controller.rb
+++ app/controllers/garfield_controller.rb
@@ -6,7 +6,7 @@ class GarfieldController < ApplicationController
   before_action :require_login
 
   def show
-    local_path = "#{Rails.root}/tmp/_garfield_#{params[:name]}.jpg"
+    local_path = "#{Rails.root}/tmp/_garfield_#{params[:name]}.jpg".freeze
     send_file(local_path, disposition: 'inline', type: 'image/jpeg', x_sendfile: true)
   end
 end

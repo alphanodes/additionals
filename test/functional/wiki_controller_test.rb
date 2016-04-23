1fc8049e2f40b18edb819aa6f333b0c0bf2b4f03
Alexander Meindl <a.meindl@alphanodes.com>
2016-04-23 14:22:24 +0200

    Fix tests

--- test/functional/wiki_controller_test.rb
+++ test/functional/wiki_controller_test.rb
@@ -159,4 +159,50 @@ class WikiControllerTest < ActionController::TestCase
     assert_template 'show'
     assert_select 'div.wiki span.current-date', Time.zone.today.cweek.to_s
   end
+
+  def test_show_user_with_id
+    @request.session[:user_id] = 1
+    @page.content.text = '{{user(1)}}'
+    @page.content.save!
+    get :show, project_id: 1, id: @page_name
+    assert_response :success
+    assert_template 'show'
+    assert_select 'a[href=?]', '/users/1',
+                  text: 'admin'
+  end
+
+  def test_show_user_with_id_fullname
+    @request.session[:user_id] = 1
+    @page.content.text = '{{user(1, format=firstname_lastname)}}'
+    @page.content.save!
+    get :show, project_id: 1, id: @page_name
+    assert_response :success
+    assert_template 'show'
+    assert_select 'a.user', text: 'Redmine Admin'
+    assert_select 'a[href=?]', '/users/1',
+                  text: 'Redmine Admin'
+  end
+
+  def test_show_user_with_name
+    @request.session[:user_id] = 2
+    @page.content.text = '{{user(jsmith)}}'
+    @page.content.save!
+    get :show, project_id: 1, id: @page_name
+    assert_response :success
+    assert_template 'show'
+    assert_select 'a[href=?]', '/users/2',
+                  text: 'jsmith'
+  end
+
+  def test_show_user_with_name_fullname
+    @request.session[:user_id] = 2
+    @page.content.text = '{{user(jsmith, format=firstname_lastname, avatar=true)}}'
+    @page.content.save!
+    get :show, project_id: 1, id: @page_name
+    assert_response :success
+    assert_template 'show'
+    assert_select 'a.user', text: 'John Smith'
+    assert_select 'a[href=?]', '/users/2',
+                  text: 'John Smith'
+  end
 end

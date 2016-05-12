8aeea1f8856cf42efdd4a5f13d69817a30f7034f
Alexander Meindl <a.meindl@alphanodes.com>
2016-05-12 18:07:41 +0200

    updated bootstrap-datepicker and fixed zh locale problem #30 next try

--- lib/redmine_tweaks/helpers/tweaks_helper.rb
+++ lib/redmine_tweaks/helpers/tweaks_helper.rb
@@ -107,7 +107,6 @@ module RedmineTweaks
     def bootstrap_datepicker_locale
       s = ''
       locale = User.current.language.blank? ? ::I18n.locale : User.current.language
-      locale = 'zh-CN' if locale == 'zh'
       s = javascript_include_tag("locales/bootstrap-datepicker.#{locale}.min.js", plugin: 'redmine_tweaks') unless locale == 'en'
       s
     end

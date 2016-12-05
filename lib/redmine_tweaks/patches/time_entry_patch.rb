508435362b0b358f25707cdeffbe15b8e2bb9d0a
Alexander Meindl <a.meindl@alphanodes.com>
2016-12-05 20:31:46 +0100

    No edit and delete actions possible on closed issues #33

--- lib/redmine_tweaks/patches/time_entry_patch.rb
+++ lib/redmine_tweaks/patches/time_entry_patch.rb
@@ -9,6 +9,7 @@ module RedmineTweaks
       def self.included(base)
         base.send(:include, InstanceMethods)
         base.class_eval do
+          alias_method_chain :editable_by?, :tweaks
           validate :validate_issue_allowed
         end
       end
@@ -18,6 +19,12 @@ module RedmineTweaks
           return unless issue_id && issue
           return if Setting.commit_logtime_enabled? && (issue.updated_on + 3.seconds) > Time.zone.now
           errors.add(:issue_id, :issue_log_time_not_allowed) unless issue.log_time_allowed?
+        end
+
+        def editable_by_with_tweaks?(usr)
+          return false unless editable_by_without_tweaks?(usr)
+          return true unless issue_id && issue
+          issue.log_time_allowed?
         end
       end
     end

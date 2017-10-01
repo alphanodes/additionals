ba6628d7b0a62b05c22d2fe54e0d652c690246ed
Alexander Meindl <a.meindl@alphanodes.com>
2017-10-01 07:39:26 +0200

    Updated deface checksum for Redmine 3.4

--- app/overrides/addto_wiki_show.rb
+++ app/overrides/addto_wiki_show.rb
@@ -1,5 +1,5 @@
 Deface::Override.new virtual_path:  'wiki/show',
                      name:          'addto-wiki-show',
                      insert_before: 'div.contextual',
-                     original:      '27f94f15c1fa6fba3195f948a0901f3cf2cbfc06',
+                     original:      'add94a807567d348a276d12ad25929e5e9bb03d6',
                      partial:       'wiki/show_additionals'

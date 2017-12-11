c2a34f57329c6f3eff5dd44b663f743ac23d21a3
Alexander Meindl <a.meindl@alphanodes.com>
2017-12-11 22:10:49 +0100

    js cleanups

--- assets/javascripts/additionals_to_select2.js
+++ assets/javascripts/additionals_to_select2.js
@@ -10,9 +10,10 @@ function filterAdditionalsFormatState (opt) {
   return $opt;
 }
 
+/* global availableFilters additionals_field_formats additionals_filter_urls:true* */
 function additionals_transform_to_select2(field){
-  field_format = availableFilters[field]['field_format'];
-  initialized_select2 = $('#tr_' + field + ' .values .select2');
+  var field_format = availableFilters[field]['field_format'];
+  var initialized_select2 = $('#tr_' + field + ' .values .select2');
   if (initialized_select2.size() == 0 && $.inArray(field_format, additionals_field_formats) >= 0) {
     $('#tr_' + field + ' .toggle-multiselect').hide();
     $('#tr_' + field + ' .values .value').attr('multiple', 'multiple');
@@ -21,10 +22,10 @@ function additionals_transform_to_select2(field){
         url: additionals_filter_urls[field_format],
         dataType: 'json',
         delay: 250,
-        data: function (params) {
+        data: function(params) {
           return { q: params.term };
         },
-        processResults: function (data, params) {
+        processResults: function(data, params) {
           return { results: data };
         },
         cache: true

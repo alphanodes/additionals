23ac4bf36308a7c3ae26bdfad6ca8076535a347c
Alexander Meindl <a.meindl@alphanodes.com>
2018-02-17 16:14:19 +0100

    comment update

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 # See: http://guides.rubyonrails.org/routing.html
 
 # Don't create routes for repositories resources with only: []
-# to not override Redmine's routes.
+# do not override Redmine's routes.
 resources :issues, only: [] do
   resource 'assign_to_me', only: %i[update], controller: 'additionals_assign_to_me'
   resource 'change_status', only: %i[update], controller: 'additionals_change_status'

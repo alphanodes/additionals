1e600d451148fc608730a722e8b2374d46d62e85
Alexander Meindl <alex@alphanodes.com>
2018-04-15 18:24:11 +0200

    Update deface checksum for base layout

--- app/overrides/layouts/base.rb
+++ app/overrides/layouts/base.rb
@@ -1,5 +1,5 @@
 Deface::Override.new virtual_path:  'layouts/base',
                      name:          'add-body-header',
                      insert_before: 'div#wrapper',
-                     original:      '1f01a1cbf472bdc2b9b46bf5b8ace23a87e71663',
+                     original:      '4af81ed701989727953cea2e376c9d83665d7eb2',
                      partial:       'additionals/global_body_header'

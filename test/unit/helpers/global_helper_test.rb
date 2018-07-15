f62d57e4a8599f86c9154603ede9430a0a1c8469
Alexander Meindl <alex@alphanodes.com>
2018-07-15 10:38:20 +0200

    more travis fixes

--- test/unit/helpers/global_helper_test.rb
+++ test/unit/helpers/global_helper_test.rb
@@ -1,4 +1,4 @@
-require File.expand_path('../../test_helper', __dir__)
+require File.expand_path('../../../test_helper', __FILE__)
 
 class GlobalHelperTest < Redmine::HelperTest
   include ApplicationHelper

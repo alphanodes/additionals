c9afc3c068c255c699eaaafa0bf4d2f34b8b238f
Alexander Meindl <alex@alphanodes.com>
2018-07-18 06:47:47 +0200

    Optmize css prio class

--- lib/additionals/patches/issue_priority_patch.rb
+++ lib/additionals/patches/issue_priority_patch.rb
@@ -11,31 +11,31 @@ module Additionals
 
       module InstanceMethods
         def css_classes_with_additionals
-          classes = [css_classes_without_additionals]
-          name_class = css_name_based_class
-          classes << name_class if name_class.present?
+          classes = [css_classes_without_additionals, css_name_based_class]
           classes.join(' ')
         end
 
         # css class based on priority name
         def css_name_based_class
-          name_classes = []
-          name_classes << { name: 'prio-name-low',
-                            words: [l(:default_priority_low), 'Low', 'Trivial', 'Niedrig'] }
-          name_classes << { name: 'prio-name-normal',
-                            words: [l(:default_priority_normal), 'Normal', 'Minor', 'Unwesentlich', 'Default'] }
-          name_classes << { name: 'prio-name-high',
-                            words: [l(:default_priority_high), 'High', 'Major', 'Schwer', 'Hoch'] }
-          name_classes << { name: 'prio-name-urgent',
-                            words: [l(:default_priority_urgent), 'Urgent', 'Critical', 'Kritisch', 'Dringend'] }
-          name_classes << { name: 'prio-name-immediate',
-                            words: [l(:default_priority_immediate), 'Immediate', 'Blocker', 'Very high', 'Jetzt'] }
-
-          name_classes.each do |name_class|
+          css_name_based_classes.each do |name_class|
             return name_class[:name] if name_class[:words].any? { |s| s.casecmp(name).zero? }
           end
+          'prio-name-other'
+        end
 
-          nil
+        def css_name_based_classes
+          @css_name_based_classes ||= [{ name: 'prio-name-low',
+                                         words: [l(:default_priority_low), 'Low', 'Trivial', 'Niedrig'] },
+                                       { name: 'prio-name-normal',
+                                         words: [l(:default_priority_normal), 'Normal', 'Minor', 'Unwesentlich', 'Default'] },
+                                       { name: 'prio-name-normal',
+                                         words: [l(:default_priority_normal), 'Normal', 'Minor', 'Unwesentlich', 'Default'] },
+                                       { name: 'prio-name-high',
+                                         words: [l(:default_priority_high), 'High', 'Major', 'Schwer', 'Hoch'] },
+                                       { name: 'prio-name-urgent',
+                                         words: [l(:default_priority_urgent), 'Urgent', 'Critical', 'Kritisch', 'Dringend'] },
+                                       { name: 'prio-name-immediate',
+                                         words: [l(:default_priority_immediate), 'Immediate', 'Blocker', 'Very high', 'Jetzt'] }]
         end
       end
     end

f34724615e3a3d1a61e39b286320f1971ef57149
Alexander Meindl <alex@alphanodes.com>
2018-07-24 07:41:16 +0200

    restore settings after changing them

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -25,12 +25,15 @@ end
 module Additionals
   module TestHelper
     def with_additionals_settings(settings, &_block)
+      saved_settings = Setting.plugin_additionals.dup
       new_settings = Setting.plugin_additionals.dup
       settings.each do |key, value|
         new_settings[key] = value
       end
       Setting.plugin_additionals = new_settings
       yield
+    ensure
+      Setting.plugin_additionals = saved_settings
     end
 
     def prepare_tests

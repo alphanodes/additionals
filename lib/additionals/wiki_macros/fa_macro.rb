c148c862949a0af9ebf3f54b6f3e4c8e2d7e48e7
Alexander Meindl <alex@alphanodes.com>
2018-08-30 17:39:57 +0200

    remove space

--- lib/additionals/wiki_macros/fa_macro.rb
+++ lib/additionals/wiki_macros/fa_macro.rb
@@ -57,10 +57,10 @@ module Additionals
 
         if options[:link].present?
           content_tag(:a, href: options[:link]) do
-            content_tag(:i, options[:text], content_options)
+            content_tag(:i, ' ' + options[:text], content_options)
           end
         else
-          content_tag(:i, options[:text], content_options)
+          content_tag(:i, ' ' + options[:text], content_options)
         end
       end
     end

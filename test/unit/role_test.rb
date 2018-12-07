77914b4ce02c9c431cbb882d01403ef776c73d3c
Alexander Meindl <alex@alphanodes.com>
2018-12-07 02:39:35 +0100

    Add base role tests with hide

---
+++ test/unit/role_test.rb
@@ -0,0 +1,35 @@
+require File.expand_path('../../test_helper', __FILE__)
+
+class RoleTest < Additionals::TestCase
+  fixtures :projects, :users, :members, :member_roles, :roles,
+           :trackers, :projects_trackers,
+           :enabled_modules,
+           :roles
+
+  def setup
+    prepare_tests
+  end
+
+  def test_create
+    role = Role.new(name: 'role without hide')
+    assert role.save
+
+    role.reload
+    assert_equal false, role.hide
+
+    role = Role.new(name: 'role with hide', hide: true)
+    assert role.save
+
+    role.reload
+    assert_equal true, role.hide
+  end
+
+  def test_edit
+    role = roles(:roles_001)
+    role.hide = true
+    assert role.save
+
+    role.reload
+    assert_equal true, role.hide
+  end
+end

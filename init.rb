c5ae5c7af68e63e63025a5e6bede6a3525380c92
Ko Nagase <nagase@georepublic.co.jp>
2019-03-13 11:16:56 +0900

    Support 'rake db:create' command for creating db
    fixed rubocop errors

--- init.rb
+++ init.rb
@@ -38,19 +38,23 @@ Redmine::Plugin.register :additionals do
   RedCloth3::ALLOWED_TAGS << 'div'
 end
 
-if ActiveRecord::Base.connection.table_exists?(:settings)
-  Rails.configuration.to_prepare do
-    Additionals.setup
-  end
+begin
+  if ActiveRecord::Base.connection.table_exists?(:settings)
+    Rails.configuration.to_prepare do
+      Additionals.setup
+    end
 
-  Rails.application.config.after_initialize do
-    FONTAWESOME_ICONS = { fab: AdditionalsFontAwesome.load_icons(:fab),
-                          far: AdditionalsFontAwesome.load_icons(:far),
-                          fas: AdditionalsFontAwesome.load_icons(:fas) }.freeze
-  end
+    Rails.application.config.after_initialize do
+      FONTAWESOME_ICONS = { fab: AdditionalsFontAwesome.load_icons(:fab),
+                            far: AdditionalsFontAwesome.load_icons(:far),
+                            fas: AdditionalsFontAwesome.load_icons(:fas) }.freeze
+    end
 
-  Rails.application.paths['app/overrides'] ||= []
-  Dir.glob(Rails.root.join('plugins', '*', 'app', 'overrides')).each do |dir|
-    Rails.application.paths['app/overrides'] << dir unless Rails.application.paths['app/overrides'].include?(dir)
+    Rails.application.paths['app/overrides'] ||= []
+    Dir.glob(Rails.root.join('plugins', '*', 'app', 'overrides')).each do |dir|
+      Rails.application.paths['app/overrides'] << dir unless Rails.application.paths['app/overrides'].include?(dir)
+    end
   end
+rescue ActiveRecord::NoDatabaseError
+  puts 'additionals - no database'
 end

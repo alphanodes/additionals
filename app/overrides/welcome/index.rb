d138e5e6e18100de74fb70ae1f09baf09a9d4fc3
Alexander Meindl <alex@alphanodes.com>
2019-12-25 14:37:45 +0100

    #68 fix welcome page positions for Redmine 4.1

--- app/overrides/welcome/index.rb
+++ app/overrides/welcome/index.rb
@@ -1,13 +1,13 @@
 Deface::Override.new virtual_path: 'welcome/index',
-                     name: 'view-welcome-index-bottom-hook',
-                     insert_after: 'div.splitcontentright',
-                     original: 'dd470844bcaa4d7c9dc66e70e6c0c843d42969bf',
-                     partial: 'hooks/view_welcome_index_bottom'
-Deface::Override.new virtual_path: 'welcome/index',
                      name: 'add-welcome-top-content',
-                     insert_before: 'div.splitcontentleft',
+                     insert_before: "div.#{Redmine::VERSION.to_s >= '4.1' ? 'splitcontent' : 'splitcontentleft'}",
                      original: 'e7de0a2e88c5ccb4d1feb7abac239e4b669babed',
                      partial: 'welcome/overview_top'
+Deface::Override.new virtual_path: 'welcome/index',
+                     name: 'view-welcome-index-bottom-hook',
+                     insert_after: "div.#{Redmine::VERSION.to_s >= '4.1' ? 'splitcontent' : 'splitcontentright'}",
+                     original: 'dd470844bcaa4d7c9dc66e70e6c0c843d42969bf',
+                     partial: 'hooks/view_welcome_index_bottom'
 Deface::Override.new virtual_path: 'welcome/index',
                      name: 'remove-welcome-news',
                      replace: 'div.news',

aeeecedd88f986c5cd68d7e62f3d997bac327053
Alexander Meindl <alex@alphanodes.com>
2019-12-28 14:23:08 +0100

    Fix project overview page for Redmine 4.1

--- app/overrides/projects/show.rb
+++ app/overrides/projects/show.rb
@@ -1,5 +1,5 @@
 Deface::Override.new virtual_path: 'projects/show',
                      name: 'view-projects-show-bottom-hook',
-                     insert_after: 'div.splitcontentright',
+                     insert_after: "div.#{Redmine::VERSION.to_s >= '4.1' ? 'splitcontent' : 'splitcontentright'}",
                      original: '8cd4d1b38e8afcb4665dbfea661b7048fbd92cf7',
                      partial: 'hooks/view_projects_show_bottom'

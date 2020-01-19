aed85b0065c15d12f77735c56e49ff3c67ab4c04
Alexander Meindl <alex@alphanodes.com>
2020-01-19 08:41:55 +0100

    Allow admin to copy, archive and delete project from project overview

--- app/overrides/projects/show.rb
+++ app/overrides/projects/show.rb
@@ -3,3 +3,10 @@ Deface::Override.new virtual_path: 'projects/show',
                      insert_after: "div.#{Redmine::VERSION.to_s >= '4.1' ? 'splitcontent' : 'splitcontentright'}",
                      original: '8cd4d1b38e8afcb4665dbfea661b7048fbd92cf7',
                      text: '<%= call_hook(:view_projects_show_bottom, project: @project) %>'
+if Redmine::VERSION.to_s >= '4.1'
+  Deface::Override.new virtual_path: 'projects/show',
+                       name: 'view-projects-actions-dropdown-hook',
+                       insert_before: 'erb[loud]:contains("link_to_if_authorized l(:label_settings)")',
+                       original: 'da5a3461fab48e7198701c3e7e5a8e98295d9e3a',
+                       text: '<%= call_hook(:view_projects_show_actions_dropdown, project: @project) %>'
+end

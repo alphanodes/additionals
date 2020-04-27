4c072e0b5f9386403e3b47433d6d24272d44b918
Alexander Meindl <alex@alphanodes.com>
2020-04-27 06:53:26 +0200

    Another solution for css problem with https://www.redmine.org/issues/33339

--- app/overrides/settings/plugin.rb
+++
@@ -1,8 +0,0 @@
-if Redmine::VERSION.to_s >= '4.1'
-  # Fix settings plugin bug starting with https://www.redmine.org/issues/33339
-  Deface::Override.new virtual_path: 'settings/plugin',
-                       name: 'fix-box-settings-plugin-bug',
-                       remove_from_attributes: 'div.box.tabular.settings',
-                       attributes: { class: 'settings' },
-                       original: '4778c00d28a60d776afc3e4b69112d6b892ec9ae'
-end

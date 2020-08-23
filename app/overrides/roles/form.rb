fbaa8af970919252409ec8358ec6cd52d9296f1a
Alexander Meindl <alex@alphanodes.com>
2020-08-23 14:03:30 +0200

    Adjust deface checksum

--- app/overrides/roles/form.rb
+++ app/overrides/roles/form.rb
@@ -1,5 +1,5 @@
 Deface::Override.new virtual_path: 'roles/_form',
                      name: 'roles-form-hide',
                      insert_before: 'p.manage_members_shown',
-                     original: 'b2a317f49e0b65ae506c8871f0c2bcc3e8098766',
+                     original: '7413482e01a07b5615be1900b974fee87224cb47',
                      partial: 'roles/additionals_form'

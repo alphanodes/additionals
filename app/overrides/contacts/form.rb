40ed393a088aaea6b4f604d3d5768bfe0e700ec1
Alexander Meindl <alex@alphanodes.com>
2020-09-17 11:11:17 +0200

    Only load contact hook, if missing

--- app/overrides/contacts/form.rb
+++ app/overrides/contacts/form.rb
@@ -1,6 +1,8 @@
-# redmine_contacts does not provide hook
-Deface::Override.new virtual_path: 'contacts/_form',
-                     name: 'contacts-form-hook',
-                     insert_bottom: 'div#contact_data',
-                     original: 'df6cae24cfd26e5299c45c427fbbd4e5f23c313e',
-                     partial: 'hooks/view_contacts_form'
+unless Redmine::Plugin.installed? 'redmine_servicedesk'
+  # redmine_contacts does not provide hook
+  Deface::Override.new virtual_path: 'contacts/_form',
+                       name: 'contacts-form-hook',
+                       insert_bottom: 'div#contact_data',
+                       original: 'df6cae24cfd26e5299c45c427fbbd4e5f23c313e',
+                       partial: 'hooks/view_contacts_form'
+end

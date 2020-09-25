ec76c85044d2a625b09522eb195c0b14a9e3ff50
Alexander Meindl <alex@alphanodes.com>
2020-09-25 10:42:03 +0200

    Journal helper extentions

--- app/helpers/additionals_journals_helper.rb
+++ app/helpers/additionals_journals_helper.rb
@@ -61,4 +61,99 @@ module AdditionalsJournalsHelper
                        title: l(:button_delete),
                        class: 'icon-only icon-del')], ' '
   end
+
+  # Returns the textual representation of a single journal detail
+  # rubocop: disable Rails/OutputSafety
+  def entity_show_detail(entity, detail, no_html = false, options = {}) # rubocop:disable Style/OptionalBooleanParameter:
+    multiple = false
+    no_detail = false
+    show_diff = false
+    label = nil
+    diff_url_method = "diff_#{entity.name.underscore}_journal_url"
+    entity_prop = entity_show_detail_prop detail, options
+
+    if entity_prop.present?
+      label = entity_prop[:label] if entity_prop.key? :label
+      value = entity_prop[:value] if entity_prop.key? :value
+      old_value = entity_prop[:old_value] if entity_prop.key? :old_value
+      show_diff = entity_prop[:show_diff] if entity_prop.key? :show_diff
+      no_detail = entity_prop[:no_detail] if entity_prop.key? :no_detail
+    end
+
+    if label || show_diff
+      unless no_html
+        label = tag.strong(label)
+        old_value = tag.i(old_value) if detail.old_value
+        old_value = tag.del(old_value) if detail.old_value && detail.value.blank?
+        value = tag.i(value) if value
+      end
+
+      html =
+        if no_detail
+          l(:text_journal_changed_no_detail, label: label)
+        elsif show_diff
+          s = l(:text_journal_changed_no_detail, label: label)
+          unless no_html
+            diff_link = link_to l(:label_diff),
+                                send(diff_url_method,
+                                     detail.journal_id,
+                                     detail_id: detail.id,
+                                     only_path: options[:only_path]),
+                                title: l(:label_view_diff)
+            s << " (#{diff_link})"
+          end
+          s.html_safe
+        elsif detail.value.present?
+          if detail.old_value.present?
+            l(:text_journal_changed, label: label, old: old_value, new: value)
+          elsif multiple
+            l(:text_journal_added, label: label, value: value)
+          else
+            l(:text_journal_set_to, label: label, value: value)
+          end
+        else
+          l(:text_journal_deleted, label: label, old: old_value).html_safe
+        end
+      html.html_safe
+    else
+      # default implementation for journal detail rendering
+      show_detail detail, no_html, options
+    end
+  end
+  # rubocop: enable Rails/OutputSafety
+
+  private
+
+  def entity_show_detail_prop(detail, options)
+    return options[:entity_prop] if options.key? :entity_prop
+    return unless detail.property == 'cf'
+
+    custom_field = detail.custom_field
+    return unless custom_field
+
+    return { show_diff: true, label: l(:field_description) } if custom_field.format.class.change_as_diff
+
+    case custom_field.format.name
+    when 'project_relation'
+      prop = { label: custom_field.name }
+      project = Project.visible.where(id: detail.value).first if detail.value.present?
+      old_project = Project.visible.where(id: detail.old_value).first if detail.old_value.present?
+      prop[:value] = link_to_project(project) if project.present?
+      prop[:old_value] = link_to_project(old_project) if old_project.present?
+    when 'db_entry'
+      prop = { label: custom_field.name }
+      db_entry = DbEntry.visible.where(id: detail.value).first if detail.value.present?
+      old_db_entry = DbEntry.visible.where(id: detail.old_value).first if detail.old_value.present?
+      prop[:value] = link_to(db_entry.name, db_entry_url(db_entry)) if db_entry.present?
+      prop[:old_value] = link_to(old_db_entry.name, db_entry_url(old_db_entry)) if old_db_entry.present?
+    when 'password'
+      prop = { label: custom_field.name }
+      password = Password.visible.where(id: detail.value).first if detail.value.present? && defined?(Password)
+      old_password = Password.visible.where(id: detail.old_value).first if detail.old_value.present? && defined?(Password)
+      prop[:value] = link_to(password.name, password_url(password)) if password.present?
+      prop[:old_value] = link_to(old_password.name, password_url(old_password)) if old_password.present?
+    end
+
+    prop
+  end
 end

39b45c1cdb7f9ed9e95dfb302636fc1944d421a3
Alexander Meindl <alex@alphanodes.com>
2020-09-29 09:36:19 +0200

    Fix deface checksome for contacts

--- app/overrides/contacts/form.rb
+++ app/overrides/contacts/form.rb
@@ -1,8 +1,15 @@
 unless Redmine::Plugin.installed? 'redmine_servicedesk'
-  # redmine_contacts does not provide hook
-  Deface::Override.new virtual_path: 'contacts/_form',
-                       name: 'contacts-form-hook',
-                       insert_bottom: 'div#contact_data',
-                       original: 'df6cae24cfd26e5299c45c427fbbd4e5f23c313e',
-                       partial: 'hooks/view_contacts_form'
+  if defined?(CONTACTS_VERSION_TYPE) && CONTACTS_VERSION_TYPE == 'PRO version'
+    Deface::Override.new virtual_path: 'contacts/_form',
+                         name: 'contacts-pro-form-hook',
+                         insert_bottom: 'div#contact_data',
+                         original: 'df6cae24cfd26e5299c45c427fbbd4e5f23c313e',
+                         partial: 'hooks/view_contacts_form'
+  else
+    Deface::Override.new virtual_path: 'contacts/_form',
+                         name: 'contacts-form-hook',
+                         insert_bottom: 'div#contact_data',
+                         original: '217049684a0bcd7e404dc6b5b2348aae47ac8a72',
+                         partial: 'hooks/view_contacts_form'
+  end
 end

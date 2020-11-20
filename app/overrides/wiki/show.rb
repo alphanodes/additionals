880e047fb439a0675a5cfdcb981d15f718c9a832
Alexander Meindl <alex@alphanodes.com>
2020-11-20 18:56:45 +0100

    new hooks for wiki

---
+++ app/overrides/wiki/show.rb
@@ -0,0 +1,5 @@
+Deface::Override.new virtual_path: 'wiki/show',
+                     name: 'wiki-show-bottom',
+                     insert_before: 'p.wiki-update-info',
+                     original: 'd9f52aa98f1cb335314570d3f5403690f1b29145',
+                     partial: 'hooks/view_wiki_show_bottom'

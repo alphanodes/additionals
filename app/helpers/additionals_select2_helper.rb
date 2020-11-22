589a18d821e7f51211619137c807bfda35438ba6
Alexander Meindl <alex@alphanodes.com>
2020-11-22 10:07:23 +0100

    Allow set id for select2 element

--- app/helpers/additionals_select2_helper.rb
+++ app/helpers/additionals_select2_helper.rb
@@ -1,9 +1,10 @@
 module AdditionalsSelect2Helper
   def additionals_select2_tag(name, option_tags = nil, options = {})
     s = select_tag(name, option_tags, options)
+    id = options.delete(:id) || sanitize_to_id(name)
     s << hidden_field_tag("#{name}[]", '') if options[:multiple] && options.fetch(:include_hidden, true)
 
-    s + javascript_tag("select2Tag('#{sanitize_to_id name}', #{options.to_json});")
+    s + javascript_tag("select2Tag('#{id}', #{options.to_json});")
   end
 
   # Transforms select filters of +type+ fields into select2

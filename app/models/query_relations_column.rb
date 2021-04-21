631d096abe36550ee84b2712e1b354899c3c50fa
Alexander Meindl <alex@alphanodes.com>
2021-04-21 14:57:24 +0200

    Fix namespace for query column

--- app/models/query_relations_column.rb
+++ app/models/query_relations_column.rb
@@ -1,11 +1,5 @@
 # frozen_string_literal: true
 
-# This file is a part of redmine_db,
-# a Redmine plugin to manage custom database entries.
-#
-# Copyright (c) 2016-2021 AlphaNodes GmbH
-# https://alphanodes.com
-
 class QueryRelationsColumn < QueryColumn
   # NOTE: used for CSV and PDF export
   def value_object(object)

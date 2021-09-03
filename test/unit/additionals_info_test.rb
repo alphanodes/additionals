bc9054b6150f31c071ec4c0b37a1bd6c3e217405
Alexander Meindl <alex@alphanodes.com>
2021-09-03 17:42:57 +0200

    better abstraction for system infos

--- test/unit/additionals_info_test.rb
+++ test/unit/additionals_info_test.rb
@@ -4,12 +4,12 @@ require File.expand_path '../../test_helper', __FILE__
 
 class AdditionalsInfoTest < Additionals::TestCase
   def test_system_info
-    text = AdditionalsInfo.system_info
+    text = AdditionalsInfo.new.system_info
     assert_not_equal '', text
     assert_not_equal 'unknown', text
   end
 
   def test_windows_platform
-    assert_not AdditionalsInfo.windows_platform?
+    assert_not AdditionalsInfo.new.windows_platform?
   end
 end

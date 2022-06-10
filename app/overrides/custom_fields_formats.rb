b1218577f3a49f253bae44127aac6b0b7e6497d4
Alexander Meindl <alex@alphanodes.com>
2022-06-10 17:28:26 +0200

    DEPRECATION WARNING fixed for custom_fields/formats/additionals_text

--- app/overrides/custom_fields_formats.rb
+++ app/overrides/custom_fields_formats.rb
@@ -6,5 +6,5 @@ module CustomFieldsFormats
                        replace: 'erb[silent]:contains(\'if @custom_field.class.name == "IssueCustomField"\')',
                        original: '5e0fbf8e8156bf1514cbada3dbaca9afc3c19bbb',
                        closing_selector: "erb[silent]:contains('end')",
-                       partial: 'custom_fields/formats/additionals_text.html.slim'
+                       partial: 'custom_fields/formats/additionals_text'
 end

329914c34bc896fc7d88e423795e03904340c2c9
Alexander Meindl <alex@alphanodes.com>
2023-01-03 08:32:55 +0100

    add symbol support for additionals settings label

--- app/helpers/additionals_settings_helper.rb
+++ app/helpers/additionals_settings_helper.rb
@@ -5,7 +5,6 @@ module AdditionalsSettingsHelper
     active_value = options.delete(:active_value).presence || @settings.present? && @settings[name]
     tag_name = options.delete(:tag_name).presence || "settings[#{name}]"
 
-    label_title = options.delete(:label).presence || l("label_#{name}")
     value = options.delete :value
     value_is_bool = options.delete :value_is_bool
     custom_value = if value.nil?
@@ -22,7 +21,7 @@ module AdditionalsSettingsHelper
                 RedminePluginKit.true? active_value
               end
 
-    s = [label_tag(tag_name, label_title)]
+    s = [label_tag(tag_name, additionals_settings_label(name, options))]
     s << hidden_field_tag(tag_name, 0, id: nil) if !custom_value || value_is_bool
     s << check_box_tag(tag_name, value, checked, **options)
     safe_join s
@@ -50,16 +49,11 @@ module AdditionalsSettingsHelper
 
   def additionals_settings_select(name, values, **options)
     tag_name = options.delete(:tag_name).presence || "settings[#{name}]"
-
-    label_title = [options.delete(:label).presence || l("label_#{name}")]
-    label_title << tag.span('*', class: 'required') if options[:required].present?
-
-    safe_join [label_tag(tag_name, safe_join(label_title, ' ')),
+    safe_join [label_tag(tag_name, additionals_settings_label(name, options)),
                select_tag(tag_name, values, **options)]
   end
 
   def additionals_settings_textarea(name, **options)
-    label_title = options.delete(:label).presence || l("label_#{name}")
     value = if options.key? :value
               options.delete :value
             elsif @settings.present?
@@ -69,11 +63,23 @@ module AdditionalsSettingsHelper
     options[:class] = 'wiki-edit' unless options.key? :class
     options[:rows] = addtionals_textarea_cols value unless options.key? :rows
 
-    safe_join [label_tag("settings[#{name}]", label_title),
+    safe_join [label_tag("settings[#{name}]", additionals_settings_label(name, options)),
                text_area_tag("settings[#{name}]", value, **options)]
   end
 
   private
+
+  def additionals_settings_label(name, options)
+    label = options.delete :label
+    label_title = if label.present?
+                    [label.is_a?(Symbol) ? l(label) : label]
+                  else
+                    [l("label_#{name}")]
+                  end
+
+    label_title << tag.span('*', class: 'required') if options[:required].present?
+    safe_join label_title, ' '
+  end
 
   def additionals_settings_input_field(tag_field, name, **options)
     tag_name = options.delete(:tag_name).presence || "settings[#{name}]"
@@ -87,10 +93,7 @@ module AdditionalsSettingsHelper
               default_setting
             end
 
-    label_title = [options.delete(:label).presence || l("label_#{name}")]
-    label_title << tag.span('*', class: 'required') if options[:required].present?
-
-    safe_join [label_tag(tag_name, safe_join(label_title, ' ')),
+    safe_join [label_tag(tag_name, additionals_settings_label(name, options)),
                send(tag_field, tag_name, value, **options)], ' '
   end
 end

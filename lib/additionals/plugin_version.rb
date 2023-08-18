547f343383938fd86facc3f67bf817a3a302fc63
Alexander Meindl <a.meindl@alphanodes.com>
2023-08-18 05:49:27 +0200

    version bump

--- lib/additionals/plugin_version.rb
+++ lib/additionals/plugin_version.rb
@@ -2,6 +2,6 @@
 
 module Additionals
   module PluginVersion
-    VERSION = '3.0.9-main' unless defined? Additionals::PluginVersion::VERSION
+    VERSION = '3.0.9' unless defined? Additionals::PluginVersion::VERSION
   end
 end

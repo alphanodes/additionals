1d09d8fa6eb727d659ec08c3c61fcc58a3186cee
Alexander Meindl <a.meindl@alphanodes.com>
2024-10-06 22:00:32 +0200

    version bump

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,7 @@
 # Changelog
 
+## 3.4.0
+
 ## 3.3.2
 
 - Maintenance release
--- lib/additionals/plugin_version.rb
+++ lib/additionals/plugin_version.rb
@@ -2,6 +2,6 @@
 
 module Additionals
   module PluginVersion
-    VERSION = '3.3.2' unless defined? Additionals::PluginVersion::VERSION
+    VERSION = '3.4.0-main' unless defined? Additionals::PluginVersion::VERSION
   end
 end

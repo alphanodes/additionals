48988fc81be0b8e73c6aa3c435c3395aa2f90ef4
Alexander Meindl <a.meindl@alphanodes.com>
2024-11-09 05:27:29 +0100

    Fix deface checksum for wiki edit

--- app/overrides/wiki_edit.rb
+++ app/overrides/wiki_edit.rb
@@ -1,9 +1,17 @@
 # frozen_string_literal: true
 
 module WikiEdit
-  Deface::Override.new virtual_path: 'wiki/edit',
-                       name: 'wiki-edit',
-                       insert_before: 'fieldset',
-                       original: 'ededb6cfd5adfe8a9723d00ce0ee23575c7cc44c',
-                       partial: 'hooks/view_wiki_form_bottom'
+  if Additionals.redmine6?
+    Deface::Override.new virtual_path: 'wiki/edit',
+                         name: 'wiki-edit',
+                         insert_before: 'fieldset',
+                         original: '8c202edfea8a0a74e4c3c0c4ae53e129b1d2b1ee',
+                         partial: 'hooks/view_wiki_form_bottom'
+  else
+    Deface::Override.new virtual_path: 'wiki/edit',
+                         name: 'wiki-edit',
+                         insert_before: 'fieldset',
+                         original: 'ededb6cfd5adfe8a9723d00ce0ee23575c7cc44c',
+                         partial: 'hooks/view_wiki_form_bottom'
+  end
 end

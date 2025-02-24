f5944978d243db8fdd1d5674d15909ba4bb77f3d
Alexander Meindl <a.meindl@alphanodes.com>
2025-02-24 11:29:57 +0100

    Fix deface checksum for user profile

--- app/overrides/users_show.rb
+++ app/overrides/users_show.rb
@@ -11,7 +11,7 @@ module UsersShow
     Deface::Override.new virtual_path: 'users/show',
                          name: 'user-contextual-hook',
                          insert_bottom: 'div.contextual',
-                         original: '9d6a7ad6ba0addc68c6b4f6c3b868511bc8eb542',
+                         original: '8002f57d64a423196314261150f1df0c0208c9a8',
                          partial: 'hooks/view_users_show_contextual'
   end
 end

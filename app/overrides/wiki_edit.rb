69a5b0a6fef3156c846f8f8f70d39fc8bd9af894
Alexander Meindl <a.meindl@alphanodes.com>
2026-02-09 12:14:26 +0100

    Add Redmine master compatibility for wiki-edit deface override hash after file_type_icon rename

--- app/overrides/wiki_edit.rb
+++ app/overrides/wiki_edit.rb
@@ -1,9 +1,15 @@
 # frozen_string_literal: true
 
 module WikiEdit
+  original_hash = if Redmine::VERSION::BRANCH == 'devel'
+                    '00ce4e80d56c9bfb7d743b040266a725336fe773'
+                  else
+                    '8c202edfea8a0a74e4c3c0c4ae53e129b1d2b1ee'
+                  end
+
   Deface::Override.new virtual_path: 'wiki/edit',
                        name: 'wiki-edit',
                        insert_before: 'fieldset',
-                       original: '8c202edfea8a0a74e4c3c0c4ae53e129b1d2b1ee',
+                       original: original_hash,
                        partial: 'hooks/view_wiki_form_bottom'
 end
